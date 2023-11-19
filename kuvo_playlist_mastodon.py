158f9cba9ca41ac34a77f9ba8cbfb4b5f791099a
Al Wirtes <wirtes@gmail.com>
2023-11-19 22:40:46 +0000

    Improved stability. Improved alt text.

--- kuvo_playlist_mastodon.py
+++ kuvo_playlist_mastodon.py
@@ -24,7 +24,7 @@ def read_state(file_path):
     # This assumption is possibly dangerous. Time will tell.
     except:
         state = "starting up"
-        write_state(file_path, "starting up")
+        write_state(file_path, state)
     return state
 
 
@@ -52,12 +52,16 @@ def get_current_song(playlist_url, album_art_size):
         spin_item = soup.find('tr', class_='spin-item')
         # Check if the tag with class "spin-item" is found
         if spin_item:
+            pprint(spin_item)
             # Get the value of the "data-spin" attribute
             data_spin_value = spin_item.get('data-spin')
             # Convert it to a dict
             data_spin_item = json.loads(data_spin_value)
             # Pull the time the song was played out of the <td>
             data_spin_item["time"] = spin_item.find('td', class_='spin-time').get_text(strip=True)
+            if data_spin_item["i"] is None:
+                data_spin_item["i"] = data_spin_item["s"]
+                print(f"\n\n***** No ID on the song, so I set the i value to {data_spin_item['i']}\n\n")
         else:
             data_spin_item = json.loads({"i":"notfound","a":"artist_not_found","s":"song_not_found","r":"album_not_found"})
         # Get the image source
@@ -86,7 +90,7 @@ def post_to_mastodon(current_song, server, access_token):
     )
     # Text content to post
     text_to_post = current_song["time"] + " " + current_song["s"] + " by " + current_song["a"] + " from " + current_song["r"]
-    alt_text = current_song["r"] + " by " + current_song["a"]
+    alt_text = "An image of the cover of the record album '" + current_song["r"] + "' by " + current_song["a"]
 
     # Check if there's an image included. If there is, post it
     if current_song["image_status"] == "image":
@@ -98,7 +102,7 @@ def post_to_mastodon(current_song, server, access_token):
         mastodon.status_post(text_to_post, media_ids=[media['id']])
     else:
         mastodon.status_post(text_to_post)
-    print(f"Posted ID: {current_song['i']} to Mastodon.\n")
+    print(f"\n\n***** Posted ID: {current_song['i']} to Mastodon.\n\n")
     return
 
 
@@ -110,7 +114,7 @@ if len(sys.argv) > 1:
     print (f"{working_directory} provided")
     config = get_config()
 else:
-    print("No working directory argument provided. Exiting.")
+    print("No working directory argument provided. Exiting.\n")
     sys.exit()
 
 # Iterate
@@ -126,9 +130,11 @@ while True:
     if current_song["i"] != last_post:
         # Make sure we got a good scrape of playlist page
         if current_song["i"] == "notfound":
-            print("Latest song not found.")
+            print("\n\n***** Latest song not found.\n\n")
         else:
             post_to_mastodon(current_song, config["mastodon_server"], config["mastodon_access_token"])
             write_state(state_file, current_song["i"])
+    else:
+        print("\n\n***** Song already posted.\n\n")
 
     time.sleep(config["frequency"])
